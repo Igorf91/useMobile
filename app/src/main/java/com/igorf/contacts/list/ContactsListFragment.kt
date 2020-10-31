816138bb20ee21f66b9b5141f55cccb1990b5f37
igor <igor.fds91@hotmail.com>
2020-10-31 01:42:56 -0300

    add navigation library

---
+++ app/src/main/java/com/igorf/contacts/list/ContactsListFragment.kt
@@ -0,0 +1,8 @@
+package com.igorf.contacts.list
+
+import androidx.fragment.app.Fragment
+import com.igorf.contacts.R
+
+class ContactsListFragment : Fragment(R.layout.fragment_contacts_list) {
+
+}
