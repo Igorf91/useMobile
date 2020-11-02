bcf7382fa86f11d74cf123ff8e7d0d78db96d18b
igor <igor.fds91@hotmail.com>
2020-11-01 21:26:14 -0300

    Create voice search

--- app/src/main/java/com/igorf/contacts/list/ContactsListFragment.kt
+++ app/src/main/java/com/igorf/contacts/list/ContactsListFragment.kt
@@ -1,6 +1,8 @@
 package com.igorf.contacts.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -10,6 +12,8 @@ import com.igorf.contacts.R
 import com.igorf.contacts.data.ContactVo
 import kotlinx.android.synthetic.main.fragment_contacts_list.contactsListRecyclerView
 import kotlinx.android.synthetic.main.fragment_contacts_list.searchContactListEdit
+import kotlinx.android.synthetic.main.fragment_contacts_list.voiceSearchIcon
+import java.util.Locale
 
 class ContactsListFragment : Fragment(R.layout.fragment_contacts_list) {
 
@@ -17,6 +21,10 @@ class ContactsListFragment : Fragment(R.layout.fragment_contacts_list) {
     private val listViewModel by lazy {
         ViewModelProvider(this, ContactsListViewModelFactory())
             .get(ContactsListViewModel::class.java)
+    }
+
+    companion object {
+        private const val VOICE_REQUEST_CODE = 9874
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,6 +50,18 @@ class ContactsListFragment : Fragment(R.layout.fragment_contacts_list) {
         contactsListAdapter = ContactsListAdapter(::onItemClick)
 
         contactsListRecyclerView.adapter = contactsListAdapter
+
+        voiceSearchIcon.setOnClickListener {
+            val voiceIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).also {
+                it.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                it.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            }
+
+            startActivityForResult(voiceIntent, VOICE_REQUEST_CODE)
+        }
     }
 
     private fun setupListeners() {
@@ -58,5 +78,13 @@ class ContactsListFragment : Fragment(R.layout.fragment_contacts_list) {
     private fun onItemClick(contactVo: ContactVo) {
         val detailFragment = ContactsListFragmentDirections.contactsListToContactDetail(contactVo)
         findNavController().navigate(detailFragment)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VOICE_REQUEST_CODE && data != null) {
+            val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            searchContactListEdit.setText(result?.first())
+        }
     }
 }
